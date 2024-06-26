ca73c174330abf592aba16fe243efb0ba6ca95c1
andriivoronkin93 <andrii@skaarhoj.com>
2024-06-27 01:52:56 +0300

    add tag remove

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.0
 require github.com/s00500/env_logger v0.1.29
 
 require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
--- util.go
+++ util.go
@@ -6,35 +6,32 @@ import (
 	"os/exec"
 	"strings"
 
+	"github.com/manifoldco/promptui"
 	log "github.com/s00500/env_logger"
 )
 
 func main() {
-	// CheckArgs("<directory>")
-	// directory := os.Args[1]
-	// // Opens an already existing repository.
-	// r, err := git.PlainOpen(directory)
-	// log.Should(err)
+	log.EnableLineNumbers()
+	args := os.Args
 
-	// w, err := r.Worktree()
-	// log.Should(err)
-
-	// status, err := w.Status()
-	// log.Should(err)
-
-	// fmt.Println(status)
-	log.Info("start")
-	cmd := exec.Command("git", "describe --tags")
-
-	// Get the output
-	output, err := cmd.Output()
-	if err != nil {
-		log.Errorf("Error executing command:", err)
-		return
+	if len(args) < 2 {
+		fmt.Println("Use: util [command]\n\r")
+		fmt.Println("Commands:")
+		fmt.Println("    remove - Remove a selected tag\n\r")
+		os.Exit(1)
 	}
 
-	// Print the output
-	fmt.Println(string(output))
+	// Get the output
+	switch args[1] {
+	case "remove":
+		tags := getTags()
+
+		selectedTag := promptSelect("Select tag", tags...)
+
+		log.Info(selectedTag)
+		removeTag(selectedTag)
+	default:
+	}
 }
 
 func CheckArgs(arg ...string) {
@@ -42,4 +39,57 @@ func CheckArgs(arg ...string) {
 		log.Warnf("Usage: %s %s", os.Args[0], strings.Join(arg, " "))
 		os.Exit(1)
 	}
+}
+
+func getTags() []string {
+	cmdTag := exec.Command("git", "tag", "-l")
+	outputTag, err := cmdTag.Output()
+	log.Should(err)
+	tags := string(outputTag)
+	return splitTags(tags)
+}
+
+func splitTags(tags string) []string {
+	taglist := strings.Split(tags, "\n")
+	taglist = reverseStringArray(taglist)
+	if taglist[0] == "" {
+		taglist = taglist[1:]
+	}
+	return taglist
+}
+
+func removeTag(tag string) {
+	tag = strings.TrimSpace(tag)
+	log.Info("Remove tag: ", tag)
+	cmdLocalTag := exec.Command("git", "tag", "-d", tag)
+	outputLocalTag, err := cmdLocalTag.CombinedOutput()
+	log.Should(err)
+	log.Println(string(outputLocalTag))
+
+	cmdOriginTag := exec.Command("git", "push", "origin", "--delete", tag)
+	outputOriginTag, err := cmdOriginTag.Output()
+	log.Should(err)
+	log.Println(string(outputOriginTag))
+}
+
+func reverseStringArray(arr []string) []string {
+	n := len(arr)
+	for i := 0; i < n/2; i++ {
+		arr[i], arr[n-1-i] = arr[n-1-i], arr[i]
+	}
+	return arr
+}
+
+func promptSelect(label string, answers ...string) string {
+	result := ""
+	prompt := &promptui.Select{
+		Label: label,
+		Items: answers,
+	}
+
+	_, result, err := prompt.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result
 }
